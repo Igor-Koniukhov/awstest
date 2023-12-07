0058568fee5f884d046e5fa6e2d5977ea7bedaa7
Ihor Koniukhov <koniukhov@bubuntu-lx>
2023-12-07 10:47:50 +0200

    added simple logs

--- main.go
+++ main.go
@@ -2,19 +2,24 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 )
 
 func ApiHandler(w http.ResponseWriter, r *http.Request) {
 	clientIP := r.RemoteAddr
 	fmt.Fprintf(w, "IP Address: %s", clientIP)
+	log.Println("Handler worked - send IP.")
 
 }
 
 func main() {
+	log.Println("The API has started.")
 
 	http.HandleFunc("/", ApiHandler)
 
-	http.ListenAndServe(":8080", nil)
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
 
 }
